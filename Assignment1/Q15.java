fe186b01ce32232cd8581613b45d6c9db3f76b5d
girishs007 <83824177+girishs007@users.noreply.github.com>
2021-05-07 23:10:27 +0530

    Add files via upload

---
+++ Assignment1/Q15.java
@@ -0,0 +1,19 @@
+class Q15
+{
+	public static void main(String args[])
+	{
+		int s1=10;
+		int s2=20;
+		int temp;
+		System.out.println("s1 :"+s1);
+		System.out.println("s2 :"+s2);
+		
+		temp=s1;
+		s1=s2;
+		s2=temp;
+		System.out.println("After swapping");
+		System.out.println("s1 :"+s1);
+		System.out.println("s2 :"+s2);
+		
+	}
+}
