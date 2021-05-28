a1ade03035f2399369d103a4230d3bcd9f1d29fe
girishs007 <83824177+girishs007@users.noreply.github.com>
2021-05-28 08:58:08 +0530

    Add files via upload

---
+++ Collection Assignment/b1.java
@@ -0,0 +1,11 @@
+//write a java program to insert anelement into arraylist
+import java.util.ArrayList;
+
+public class b1 {
+  public static void main(String[] args) 
+  {
+    ArrayList<String> cars = new ArrayList<String>();
+    cars.add("Volvo");
+    System.out.println(cars);
+  }
+}
